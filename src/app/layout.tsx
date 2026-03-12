3c6323afbdaa4d9bcb34dd9345e25ccce266b524
ismailulgac-23 <82471789+ismailulgac-23@users.noreply.github.com>
2026-03-12 16:45:42 +0300

    next updateed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,9 @@
 import '@/styles/tailwind.css'
 import { Metadata } from 'next'
-import { Poppins } from 'next/font/google'
 import 'rc-slider/assets/index.css'
 import ThemeProvider from './theme-provider'
 import { AuthProvider } from '@/context/AuthContext'
 import { Toaster } from 'react-hot-toast'
-
-const poppins = Poppins({
-  subsets: ['latin'],
-  display: 'swap',
-  weight: ['300', '400', '500', '600', '700'],
-})
 
 export const metadata: Metadata = {
   title: {
@@ -30,7 +23,7 @@ export const viewport = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={poppins.className}>
+    <html lang="en">
       <body className="bg-white text-neutral-900 dark:bg-neutral-900 dark:text-neutral-100">
         <ThemeProvider>
           <AuthProvider>
